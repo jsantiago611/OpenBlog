93f9e1e0d2b0b88f0bbafbf294b820867cf6d043
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2024-11-21 16:08:15 -0500

    Update site.config.ts

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -11,8 +11,8 @@ interface SiteConfig {
 
 export const siteConfig: SiteConfig = {
 	site: 'https://blog-template-gray.vercel.app/', // Write here your website url
-	author: 'Your Name', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
+	author: 'Jon James', // Site author
+	title: 'QA Stuff', // Site title.
 	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
 	lang: 'en-GB',
 	ogLocale: 'en_GB',
